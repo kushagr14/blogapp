d4c6ff39fa8e2d20570bf3b9cefe4e806773d81d
kushagr14 <klakhanpal14@gmail.com>
2024-05-17 18:10:31 +0530

    adding blogs functionality

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,15 +3,17 @@ import { useState } from "react";
 //Blogging App using Hooks
 export default function Blog(){
 
-    const [title, setTitle]= useState("");
-    const [content, setContent]= useState("");
+    // const [title, setTitle]= useState("");
+    // const [content, setContent]= useState("");
+    const [formData, setFormData] = useState({title:"", content:""});
     const [blogs, setBlogs] = useState([]);
     
     //Passing the synthetic event as argument to stop refreshing the page on submit
     function handleSubmit(e){
         e.preventDefault();
 
-        setBlogs([{title, content}, ...blogs]);
+        setBlogs([{title:formData.title, content:formData.content}, ...blogs]);
+        setFormData({title: "", content:""});
         console.log(blogs);
     }
 
@@ -30,16 +32,16 @@ export default function Blog(){
                 <Row label="Title">
                         <input className="input"
                                 placeholder="Enter the Title of the Blog here.."
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}/>
+                                value={formData.title}
+                                onChange={(e) => setFormData({title: e.target.value, content: formData.content})}/>
                 </Row >
 
                 {/* Row component to create a row for Text area field */}
                 <Row label="Content">
                         <textarea className="input content"
                                 placeholder="Content of the Blog goes here.."
-                                value={content}
-                                onChange={(e) => setContent(e.target.value)}/>
+                                value={formData.content}
+                                onChange={(e) => setFormData({title:formData.title, content: e.target.value})}/>
                 </Row >
 
                 {/* Button to submit the blog */}            
